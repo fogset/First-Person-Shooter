6ab9287640e5b21054dc36ac7b6896ef5458acc3
Tianhao Zhang <zhangth1990@hotmail.com>
2023-02-20 23:18:11 -0700

    implement a muzzle flash

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,6 +14,9 @@ public class Weapon : MonoBehaviour
     [SerializeField]
     float damage = 30f;
 
+    [SerializeField]
+    ParticleSystem muzzleFlash;
+
     void Update()
     {
         if (Input.GetButtonDown("Fire1"))
@@ -22,6 +26,17 @@ public class Weapon : MonoBehaviour
     }
 
     private void Shoot()
+    {
+        PlayMuzzleFlash();
+        ProcessRaycast();
+    }
+
+    private void PlayMuzzleFlash()
+    {
+        muzzleFlash.Play();
+    }
+
+    private void ProcessRaycast()
     {
         RaycastHit hit;
         if (
